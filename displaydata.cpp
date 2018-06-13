79f64f10ba4df0ca8bad81356ce19fb4439422a5
Christophe Daloz - De Los Rios <christophedlr@gmail.com>
2018-06-13 21:29:35 +0200

    Pool Account Data
    - Correction of bug describe in last commit

--- displaydata.cpp
+++ displaydata.cpp
@@ -34,24 +34,27 @@ void DisplayData::downloadResult(QNetworkReply* reply)
 
     if (json["status"] == true) {
         ui->lb_wallet->setText(json["data"].toObject()["account"].toString());
-        ui->lb_current_hashrate->setText(json["data"].toObject()["hashrate"].toString());
+        ui->lb_current_hashrate->setText(json["data"].toObject()["hashrate"].toString()+" H/s");
         ui->lb_balance->setText(json["data"].toObject()["balance"].toString());
         ui->lb_unconfirmed_balance->setText(json["data"].toObject()["unconfirmed_balance"].toString());
 
-        /*QJsonArray workers = json["data"].toObject()["workers"].toArray();
+        QJsonArray workers = json["data"].toObject()["workers"].toArray();
         ui->tbl_worker->setRowCount(workers.count());
 
         for (int i = 0; i < workers.count(); ++i) {
             QJsonObject worker = workers[i].toObject();
 
+            QDateTime time;
+            time.setTime_t(static_cast<int>(worker["lastshare"].toDouble()));
+
             ui->tbl_worker->setItem(i, 0, new QTableWidgetItem(worker["id"].toString()));
-            ui->tbl_worker->setItem(i, 1, new QTableWidgetItem(worker["lastshare"].toDouble()));
-            ui->tbl_worker->setItem(i, 2, new QTableWidgetItem(worker["rating"].toDouble()));
-            ui->tbl_worker->setItem(i, 3, new QTableWidgetItem(worker["hashrate"].toString()));
+            ui->tbl_worker->setItem(i, 1, new QTableWidgetItem(time.toString(Qt::SystemLocaleShortDate)));
+            ui->tbl_worker->setItem(i, 2, new QTableWidgetItem(QString::number(worker["rating"].toDouble())));
+            ui->tbl_worker->setItem(i, 3, new QTableWidgetItem(worker["hashrate"].toString()+" H/s"));
         }
 
         ui->tbl_worker->resize(500, 400);
-        ui->tbl_worker->show();*/
+        ui->tbl_worker->show();
     }
 }
 
